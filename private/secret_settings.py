df4615e241e22e0afa4dd4fe0f6a7f9be6afe5f2
Avinash Kadiyala <avinashkadiyala@Avinashs-MacBook-Pro.local>
2023-06-15 14:44:42 -0700

    added private stuff

--- private/secret_settings.py
+++ private/secret_settings.py
@@ -1,3 +1,8 @@
 # single sign on Google (will be used if provided)
 OAUTH2GOOGLE_CLIENT_ID = "974588735245-ar2du58gmvd7i955jei6b72c7v2lrqo3.apps.googleusercontent.com"
 OAUTH2GOOGLE_CLIENT_SECRET = "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"
+
+DB_USER = "appuser"
+DB_NAME = "maindb"
+DB_PASSWORD = "YT4{IKg+~=LH|j<S"
+DB_CONNECTION = "slugsells:us-west2:slugsells-db-1"
