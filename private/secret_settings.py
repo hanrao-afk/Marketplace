88aef79e50ddf3252117976d9c7b2bb3167ddc38
Avinash Kadiyala <avinashkadiyala@Avinashs-MacBook-Pro.local>
2023-06-14 21:44:18 -0700

    working on oAuth stuff

---
+++ private/secret_settings.py
@@ -0,0 +1,3 @@
+# single sign on Google (will be used if provided)
+OAUTH2GOOGLE_CLIENT_ID = "974588735245-ar2du58gmvd7i955jei6b72c7v2lrqo3.apps.googleusercontent.com"
+OAUTH2GOOGLE_CLIENT_SECRET = "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"
